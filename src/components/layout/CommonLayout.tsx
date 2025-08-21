58856c5db14aab8d6dc2dcdf2aa60b6545ed8585
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-08-21 10:32:24 +0600

    navbar and footer added

--- src/components/layout/CommonLayout.tsx
+++ src/components/layout/CommonLayout.tsx
@@ -9,9 +9,9 @@ interface IProps {
 
 export default function CommonLayout({ children }: IProps) {
   return (
-    <div className=" min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col">
       <Navbar />
-      <div className="grow-1">{children}</div>
+      <div className="grow-1 container mx-auto px-4">{children}</div>
       <Footer />
     </div>
   );
