a71ffca3bb65470cbef1673a2062f7cd5ebd8e8f
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-08-21 10:09:19 +0600

    layout created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router";
+import CommonLayout from "./components/layout/CommonLayout";
 
 function App() {
   return (
     <>
-      <h1 className="text-5xl">Hello</h1>
-      <Outlet />
+      <CommonLayout>
+        <Outlet />
+      </CommonLayout>
     </>
   );
 }
