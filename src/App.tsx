b241a293390bde5e1a594b61592de1b109248b5f
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-08-21 09:46:14 +0600

    react router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Button } from "./components/ui/button";
+import { Outlet } from "react-router";
 
 function App() {
   return (
     <>
       <h1 className="text-5xl">Hello</h1>
-      <Button>Click Me</Button>
+      <Outlet />
     </>
   );
 }
